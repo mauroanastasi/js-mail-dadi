b1ddee40f02c9f808ab535d9eab8c01d709b6b83
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-21 20:23:30 +0100

    [UPD] creo i file necessari, creo lista di azioni da eseguire per es email, inizializzo prime variabili

---
+++ script.js
@@ -0,0 +1,5 @@
+// creo le variabile 
+const emailGroup = [`bella@hotmail.com`, `marco@hotmail.it`, `rambo@gmail.com`, `giorgia@yahoo.it`, `nando@hotmail.com`];
+
+let yourEmail = (prompt(`Inserisci la tua email personale:`));
+
